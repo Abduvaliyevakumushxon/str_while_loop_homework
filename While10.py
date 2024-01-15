2aed66af0a679a4710523cbf3a2e7daa9db290f8
Abduvaliyevakumushxon <shodmonovd1985@gmail.com>
2024-01-15 21:13:23 +0500

    10-vazifa tugadi

--- While10.py
+++ While10.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    d=0
+    count=0
+    while d<len(s):
+        if s[d].isdigit() and int(s[d])%2==0:
+            count+=int(s[d])
+        d+=1
+    return count
+print(main("589765"))
